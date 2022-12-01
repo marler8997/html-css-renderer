71a0fe96a86a6d49ea8db22d8c77902add666eed
Jonathan Marler <johnnymarler@gmail.com>
2022-11-30 22:51:34 -0700

    leverage std.build.WriteFileStep for htmlidmaps.zig

--- build.zig
+++ build.zig
@@ -5,21 +5,20 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
+    const id_maps_src = b.pathJoin(&.{ b.build_root, "src", "htmlidmaps.zig"});
+    const gen_id_maps = b.addWriteFile(id_maps_src, allocIdMapSource(b.allocator));
+
     {
         const exe = b.addExecutable("lint", "src/lint.zig");
+        exe.step.dependOn(&gen_id_maps.step);
         exe.setTarget(target);
         exe.setBuildMode(mode);
         exe.install();
     }
 
-    const id_maps_src = b.pathJoin(&.{ b.build_root, "src", "htmlidmaps.zig"});
-    const gen_id_maps = GenIdMapsStep.create(b, id_maps_src);
-
     {
         const exe = b.addExecutable("playground", "src/playground.zig");
-
         exe.step.dependOn(&gen_id_maps.step);
-
         exe.setTarget(target);
         exe.setBuildMode(mode);
         exe.install();
@@ -42,58 +41,47 @@ pub fn build(b: *std.build.Builder) void {
     }
 }
 
-const GenIdMapsStep = struct {
-    step: std.build.Step,
-    out_file: []const u8,
-    pub fn create(b: *std.build.Builder, out_file: []const u8) *GenIdMapsStep {
-        const step = b.allocator.create(GenIdMapsStep) catch unreachable;
-        step.* = .{
-            .step = std.build.Step.init(.custom, "generate id maps", b.allocator, make),
-            .out_file = out_file,
-        };
-        return step;
-    }
-    fn make(step: *std.build.Step) !void {
-        const self = @fieldParentPtr(GenIdMapsStep, "step", step);
+fn allocIdMapSource(allocator: std.mem.Allocator) []const u8 {
+    var src = std.ArrayList(u8).init(allocator);
+    errdefer src.deinit();
+    writeIdMapSource(src.writer()) catch unreachable;
+    return src.toOwnedSlice();
+}
 
-        // TODO: cache this result
-        var file = try std.fs.cwd().createFile(self.out_file, .{});
-        defer file.close();
-        const out = file.writer();
-        try out.writeAll(
-            \\const std = @import("std");
-            \\const htmlid = @import("htmlid.zig");
-            \\pub const tag_id_map = blk: {
-            \\    @setEvalBranchQuota(3000);
-            \\    break :blk std.ComptimeStringMap(htmlid.TagId, .{
-            \\
-        );
-        inline for (@typeInfo(htmlid.TagId).Enum.fields) |field| {
-            var lower_buf: [field.name.len]u8 = undefined;
-            for (field.name) |c, i| {
-                lower_buf[i] = std.ascii.toLower(c);
-            }
-            try out.print("        .{{ \"{s}\", .{} }},\n", .{lower_buf, std.zig.fmtId(field.name)});
+fn writeIdMapSource(writer: anytype) !void {
+    try writer.writeAll(
+        \\const std = @import("std");
+        \\const htmlid = @import("htmlid.zig");
+        \\pub const tag_id_map = blk: {
+        \\    @setEvalBranchQuota(3000);
+        \\    break :blk std.ComptimeStringMap(htmlid.TagId, .{
+        \\
+    );
+    inline for (@typeInfo(htmlid.TagId).Enum.fields) |field| {
+        var lower_buf: [field.name.len]u8 = undefined;
+        for (field.name) |c, i| {
+            lower_buf[i] = std.ascii.toLower(c);
         }
-        try out.writeAll(
-            \\    });
-            \\};
-            \\pub const attr_id_map = blk: {
-            \\    @setEvalBranchQuota(4000);
-            \\    break :blk std.ComptimeStringMap(htmlid.AttrId, .{
-            \\
-        );
-        inline for (@typeInfo(htmlid.AttrId).Enum.fields) |field| {
-            var lower_buf: [field.name.len]u8 = undefined;
-            for (field.name) |c, i| {
-                lower_buf[i] = std.ascii.toLower(c);
-            }
-            try out.print("        .{{ \"{s}\", .{} }},\n", .{lower_buf, std.zig.fmtId(field.name)});
-        }
-        try out.writeAll(
-            \\    });
-            \\};
-            \\
-        );
+        try writer.print("        .{{ \"{s}\", .{} }},\n", .{lower_buf, std.zig.fmtId(field.name)});
     }
-};
+    try writer.writeAll(
+        \\    });
+        \\};
+        \\pub const attr_id_map = blk: {
+        \\    @setEvalBranchQuota(4000);
+        \\    break :blk std.ComptimeStringMap(htmlid.AttrId, .{
+        \\
+    );
+    inline for (@typeInfo(htmlid.AttrId).Enum.fields) |field| {
+        var lower_buf: [field.name.len]u8 = undefined;
+        for (field.name) |c, i| {
+            lower_buf[i] = std.ascii.toLower(c);
+        }
+        try writer.print("        .{{ \"{s}\", .{} }},\n", .{lower_buf, std.zig.fmtId(field.name)});
+    }
+    try writer.writeAll(
+        \\    });
+        \\};
+        \\
+    );
+}
